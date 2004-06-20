c3941900240a8780564bf44549a179d4c6ffcc97
Shashank Bellary <sbellary@users.sourceforge.net>
2004-06-20 03:57:40 +0000

    Added script to find out if JAVA_HOME is already set

--- main/startup.cmd
+++ main/startup.cmd
@@ -2,12 +2,12 @@
 rem TODO: use libs from build directory
 rem TODO: create bootstrap.jar and remove /web-inf/classes
 
-set JAVA_HOME=C:\j2sdk1.4.2_04
+if "%JAVA_HOME%" == "" set JAVA_HOME=:\j2sdk1.4.2_04
 set JMANAGE_PORT=9090
 set JMANAGE_ROOT=./build
 
 
-set LIB_JARS=./lib/javax.servlet.jar;./lib/org.mortbay.jetty.jar;./lib/jasper-compiler.jar;./lib/jasper-runtime.jar;./lib/ant.jar
+set LIB_JARS=./lib/javax.servlet.jar;./lib/org.mortbay.jetty.jar;./lib/jasper-compiler.jar;./lib/jasper-runtime.jar;./lib/ant.jar;./lib/jdom.jar
 set EXT_JARS=./ext/weblogic61.jar
 
 set CLASSPATH=%LIB_JARS%;%EXT_JARS%;./build/WEB-INF/classes
