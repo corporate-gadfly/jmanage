38cd5c13843a8aa254cfe3ee9d0e316b2c1c8ee2
Shashank Bellary <sbellary@users.sourceforge.net>
2004-08-07 02:51:15 +0000

    Fixed issues in shell scripts

--- main/scripts/keygen.sh
+++ main/scripts/keygen.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ -n $JAVA_HOME ]; then
+if [ ! -n "$JAVA_HOME" ]; then
 	echo "Please set JAVA_HOME environment variable. JAVA_HOME must point to a JDK 1.4 installation directory."
     exit 0
 fi
 
-if [ -n $JMANAGE_HOME ]; then
+if [ ! -n "$JMANAGE_HOME" ]; then
 	echo "Please set JMANAGE_HOME environment variable pointing to jManage installation directory."
     exit 0
 fi
 
-if [ -n $JMANAGE_LIB_ROOT ]; then
+if [ ! -n "$JMANAGE_LIB_ROOT" ]; then
 	JMANAGE_LIB_ROOT=$JMANAGE_HOME
 fi
 
-JMANAGE_CLASSPATH=$JMANAGE_HOME/classes;$JMANAGE_LIB_ROOT/lib/jdom.jar
+JMANAGE_CLASSPATH=$JMANAGE_HOME/classes:$JMANAGE_LIB_ROOT/lib/jdom.jar
 
 java -classpath $JMANAGE_CLASSPATH -Djmanage.root=$JMANAGE_HOME org.jmanage.core.tools.EncryptedKeyGenerator
