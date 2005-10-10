2a7d11b188e94f10f26e6b1e232de3db8d3979cc
Rakesh Kalra <rkalra@users.sourceforge.net>
2005-10-10 20:44:56 +0000

    Fix for [1309344] unable to start jmanage

--- jmanage/testapp/bin/startTestApp.cmd
+++ jmanage/testapp/bin/startTestApp.cmd
@@ -8,6 +8,6 @@ if "%JMANAGE_CLASSPATH%" == "" goto finish
 
 set JSR160_JARS=%JMANAGE_HOME%/modules/jsr160/jmxremote-1_0_1.jar;%JMANAGE_HOME%/modules/jsr160/jmxremote_optional.jar
 
-%JAVA_HOME%/bin/java -ea -classpath %JMANAGE_CLASSPATH%;%JSR160_JARS% %DEBUG_OPTIONS% org.jmanage.testapp.jsr160.Startup %*
+"%JAVA_HOME%/bin/java" -ea -classpath %JMANAGE_CLASSPATH%;%JSR160_JARS% %DEBUG_OPTIONS% org.jmanage.testapp.jsr160.Startup %*
 
 :finish
