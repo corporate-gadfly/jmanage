d8217e3cbf1cce6dcf3e3f045a089b359c63be0d
Rakesh Kalra <rkalra@users.sourceforge.net>
2006-01-16 18:19:28 +0000

    Bug # 1407550: Websphere: The URL shows up as "null" under Managed Apps

--- modules/snmp/src/main/java/org/jmanage/core/modules/snmp/SNMPApplicationConfig.java
+++ modules/snmp/src/main/java/org/jmanage/core/modules/snmp/SNMPApplicationConfig.java
@@ -25,9 +25,4 @@ public class SNMPApplicationConfig extends ApplicationConfig{
         super();
         setPort(new Integer(161));
     }
-
-    public String getURL() {
-        return "";
-    }
-
 }
