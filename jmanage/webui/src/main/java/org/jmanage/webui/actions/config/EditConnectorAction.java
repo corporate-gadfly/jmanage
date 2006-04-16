6347445f0ae7021d5b0e3a62dbfc769e0a99469f
tscxdev <tscster@users.sourceforge.net>
2006-04-16 05:16:13 +0000

    Connector framework changes - fixed so that no need to restart server after edit config settings

--- jmanage/webui/src/main/java/org/jmanage/webui/actions/config/EditConnectorAction.java
+++ jmanage/webui/src/main/java/org/jmanage/webui/actions/config/EditConnectorAction.java
@@ -1,18 +1,25 @@
 /*
- * Copyright 2000-2004 by Upromise Inc.
- * 117 Kendrick Street, Suite 200, Needham, MA, 02494, U.S.A.
- * All rights reserved.
+ * Copyright 2004-2005 jManage.org
  *
- * This software is the confidential and proprietary information of
- * Upromise, Inc. ("Confidential Information").  You shall not disclose
- * such Confidential Information and shall use it only in accordance with
- * the terms of an agreement between you and Upromise.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  */
 package org.jmanage.webui.actions.config;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.jmanage.connector.framework.ConnectorRegistry;
 import org.jmanage.core.alert.AlertEngine;
 import org.jmanage.core.config.ApplicationConfig;
 import org.jmanage.core.config.ApplicationConfigManager;
@@ -69,6 +76,8 @@ public class EditConnectorAction extends BaseAction {
         }
 
         config.setParamValues(paramValueMap);
+        
+        ConnectorRegistry.remove(config);
 
         ApplicationConfigManager.updateApplication(config);
 
