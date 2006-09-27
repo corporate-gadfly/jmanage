9e00d8428cbc1a728f87841240aee81190bb25d7
Rakesh Kalra <rkalra@users.sourceforge.net>
2006-09-27 00:06:36 +0000

    Removed unused variable

--- utils/src/main/java/org/jmanage/core/util/SystemProperties.java
+++ utils/src/main/java/org/jmanage/core/util/SystemProperties.java
@@ -21,6 +21,5 @@ package org.jmanage.core.util;
  * @author	Rakesh Kalra
  */
 public interface SystemProperties {
-    static final String JMANAGE_PORT = "jmanage.port";
     static final String JMANAGE_ROOT = "jmanage.root";
 }
