0b758d178049848aaa7f23f9b10f35ed48e55029
Kashyap Parikh <parikhkc@users.sourceforge.net>
2006-11-05 01:04:48 +0000

    Fixing bug # [ 1532755 ] Error on KeyGen Wrapped classpath and path variables within double.

--- jmanage/cmdui/bin/jmanage.cmd
+++ jmanage/cmdui/bin/jmanage.cmd
@@ -6,6 +6,6 @@
 call setenv.cmd
 if "%JMANAGE_CLASSPATH%" == "" goto finish
 
-"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -Djmanage.root=%JMANAGE_HOME% -Dorg.jmanage.core.management.data.formatConfig=%JMANAGE_HOME%/config/text-data-format.properties org.jmanage.cmdui.Main %*
+"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -Djmanage.root="%JMANAGE_HOME%" -Dorg.jmanage.core.management.data.formatConfig="%JMANAGE_HOME%/config/text-data-format.properties" org.jmanage.cmdui.Main %*
 
 :finish
--- jmanage/tools/bin/jmcp.cmd
+++ jmanage/tools/bin/jmcp.cmd
@@ -2,5 +2,4 @@
 @rem  Copyright 2004-2005 jManage.org. All rights reserved.
 @rem===========================================================================
 
-set JMANAGE_CLASSPATH=%1;%JMANAGE_CLASSPATH%
-
+set JMANAGE_CLASSPATH=%JMANAGE_HOME%\lib\%1;%JMANAGE_CLASSPATH%
