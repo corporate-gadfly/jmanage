45cd49839dfbfaeb0c85cfe6cc3e6d688ec22f22
Shashank Bellary <sbellary@users.sourceforge.net>
2007-01-15 03:08:53 +0000

    Bug 1613921 - script shutdown.cmd

--- jmanage/webui/bin/shutdown.cmd
+++ jmanage/webui/bin/shutdown.cmd
@@ -6,6 +6,6 @@
 call setenv.cmd
 if "%JMANAGE_CLASSPATH%" == "" goto finish
 
-"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -Djava.security.policy=java.policy -Djava.util.logging.config.file="%JMANAGE_HOME%/config/logging.properties" -Djmanage.root="%JMANAGE_HOME%" -Djava.security.auth.login.config=%JMANAGE_HOME%/config/jmanage-auth.conf -Dorg.jmanage.core.management.data.formatConfig="%JMANAGE_HOME%/config/html-data-format.properties" org.jmanage.webui.Shutdown %*
+"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -Djmanage.root=%JMANAGE_HOME% org.jmanage.webui.Shutdown %*
 
 :finish
