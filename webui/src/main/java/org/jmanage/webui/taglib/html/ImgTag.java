28147ecd9d87252e08d87841ee5a4f247509087d
Shashank Bellary <sbellary@users.sourceforge.net>
2008-01-26 15:54:33 +0000

    Added license info - shashank

--- webui/src/main/java/org/jmanage/webui/taglib/html/ImgTag.java
+++ webui/src/main/java/org/jmanage/webui/taglib/html/ImgTag.java
@@ -1,3 +1,17 @@
+/**
+ * jManage - Open Source Application Management
+ * Copyright (C) 2004-2008 jManage.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
+ */
+
 package org.jmanage.webui.taglib.html;
 
 import javax.servlet.http.HttpServletRequest;
@@ -5,6 +19,10 @@ import javax.servlet.jsp.JspException;
 
 import org.apache.taglibs.standard.lang.support.ExpressionEvaluatorManager;
 
+/**
+ * @author Shashank Bellary
+ * Date: Jan 26, 2008
+ */
 public class ImgTag extends org.apache.struts.taglib.html.ImgTag {
 	
 	public int doEndTag() throws JspException {
