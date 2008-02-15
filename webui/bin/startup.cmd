6e4fbffea4fbef69a01ef22806a82e9eb5ad53f3
Shashank Bellary <sbellary@users.sourceforge.net>
2008-02-15 21:00:28 +0000

    Added back java policy and logging system properties - shashank

--- webui/bin/startup.cmd
+++ webui/bin/startup.cmd
@@ -6,6 +6,6 @@
 call setenv.cmd
 if "%JMANAGE_CLASSPATH%" == "" goto finish
 
-"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -DJMANAGE_ROOT="%JMANAGE_HOME%" org.jmanage.webui.Startup %*
+"%JAVA_HOME%/bin/java" -ea -classpath "%JMANAGE_CLASSPATH%" %DEBUG_OPTIONS% -DJMANAGE_ROOT="%JMANAGE_HOME%" -Djava.security.policy=java.policy -Djava.util.logging.config.file="%JMANAGE_HOME%/config/logging.properties" org.jmanage.webui.Startup %*
 
 :finish
