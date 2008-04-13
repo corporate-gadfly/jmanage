1bf89ff1fa0bbfa63e3b1ed6dd3b8f80170a9038
Rakesh Kalra <rkalra@users.sourceforge.net>
2008-04-13 01:43:49 +0000

    Fixed MetaDataWriter to handle null description in MBean metadata

--- utils/src/main/java/org/jmanage/util/StringUtils.java
+++ utils/src/main/java/org/jmanage/util/StringUtils.java
@@ -19,105 +19,113 @@ import java.util.*;
 import java.net.URL;
 
 /**
- *
- * date:  Feb 4, 2005
- * @author	Rakesh Kalra
+ * 
+ * date: Feb 4, 2005
+ * 
+ * @author Rakesh Kalra
  */
 public class StringUtils {
 
-    /**
-     * Converts a csv to String[]
-     *
-     * @param csv
-     * @return
-     */
-    public static String[] csvToStringArray(String csv){
+	/**
+	 * Converts a csv to String[]
+	 * 
+	 * @param csv
+	 * @return
+	 */
+	public static String[] csvToStringArray(String csv) {
 
-        if(csv == null){
-            return null;
-        }
-        StringTokenizer tokenizer = new StringTokenizer(csv, ",");
-        String[] array = new String[tokenizer.countTokens()];
-        int index = 0;
-        while(tokenizer.hasMoreTokens()){
-            array[index ++] = tokenizer.nextToken().trim();
-        }
-        return array;
-    }
+		if (csv == null) {
+			return null;
+		}
+		StringTokenizer tokenizer = new StringTokenizer(csv, ",");
+		String[] array = new String[tokenizer.countTokens()];
+		int index = 0;
+		while (tokenizer.hasMoreTokens()) {
+			array[index++] = tokenizer.nextToken().trim();
+		}
+		return array;
+	}
 
-    /**
-     * Converts a String[] to comma delimited String
-     */
-    public static String stringArrayToCSV(String[] array){
-        StringBuffer buff = new StringBuffer();
-        for(int i=0; array != null && i<array.length; i++){
-            buff.append(array[i]);
-            if(i < array.length){
-                buff.append(",");
-            }
-        }
-        return buff.toString();
-    }
+	/**
+	 * Converts a String[] to comma delimited String
+	 */
+	public static String stringArrayToCSV(String[] array) {
+		StringBuffer buff = new StringBuffer();
+		for (int i = 0; array != null && i < array.length; i++) {
+			buff.append(array[i]);
+			if (i < array.length) {
+				buff.append(",");
+			}
+		}
+		return buff.toString();
+	}
 
-    public static String[] listToStringArray(List list){
-        String[] output = new String[list.size()];
-        int i=0;
-        for(Iterator it=list.iterator(); it.hasNext(); i++){
-            output[i] = it.next().toString();
-        }
-        return output;
-    }
+	public static String[] listToStringArray(List list) {
+		String[] output = new String[list.size()];
+		int i = 0;
+		for (Iterator it = list.iterator(); it.hasNext(); i++) {
+			output[i] = it.next().toString();
+		}
+		return output;
+	}
 
-    public static List csvToList(String csv) {
-        if(csv == null){
-            return null;
-        }
-        StringTokenizer tokenizer = new StringTokenizer(csv, ",");
-        List<String> list = new ArrayList<String>(tokenizer.countTokens());
-        while(tokenizer.hasMoreTokens()){
-            list.add(tokenizer.nextToken().trim());
-        }
-        return list;
-    }
+	public static List csvToList(String csv) {
+		if (csv == null) {
+			return null;
+		}
+		StringTokenizer tokenizer = new StringTokenizer(csv, ",");
+		List<String> list = new ArrayList<String>(tokenizer.countTokens());
+		while (tokenizer.hasMoreTokens()) {
+			list.add(tokenizer.nextToken().trim());
+		}
+		return list;
+	}
 
-    public static String toString(URL[] urls){
-        StringBuffer buff = new StringBuffer();
-        for(int i=0; i<urls.length; i++){
-            buff.append(urls[i].toString());
-            buff.append(";");
-        }
-        return buff.toString();
-    }
+	public static String toString(URL[] urls) {
+		StringBuffer buff = new StringBuffer();
+		for (int i = 0; i < urls.length; i++) {
+			buff.append(urls[i].toString());
+			buff.append(";");
+		}
+		return buff.toString();
+	}
 
-    public static String padRight(String str, int totalLength) {
-        int strLen = str.length();
-        StringBuffer buff = new StringBuffer(str);
-        for(int i=0;i<totalLength - strLen; i++){
-            buff.append(' ');
-        }
-        return buff.toString();
-    }
+	public static String padRight(String str, int totalLength) {
+		int strLen = str.length();
+		StringBuffer buff = new StringBuffer(str);
+		for (int i = 0; i < totalLength - strLen; i++) {
+			buff.append(' ');
+		}
+		return buff.toString();
+	}
 
-    public static String getCharSeries(char ch, int length) {
-        char[] series = new char[length];
-        Arrays.fill(series, ch);
-        return new String(series);
-    }
+	public static String getCharSeries(char ch, int length) {
+		char[] series = new char[length];
+		Arrays.fill(series, ch);
+		return new String(series);
+	}
 
-    public static String htmlEscape(String str){
-        StringBuffer buff = new StringBuffer(str.length());
-        for(int i=0; i<str.length(); i++){
-            final char ch = str.charAt(i);
-            if(ch == '"'){
-                buff.append("&quot;");
-            }else if(ch == '<'){
-                buff.append("&lt;");
-            }else if(ch == '>'){
-                buff.append("&gt;");
-            }else{
-                buff.append(ch);
-            }
-        }
-        return buff.toString();
-    }
+	public static String htmlEscape(String str) {
+		StringBuffer buff = new StringBuffer(str.length());
+		for (int i = 0; i < str.length(); i++) {
+			final char ch = str.charAt(i);
+			if (ch == '"') {
+				buff.append("&quot;");
+			} else if (ch == '<') {
+				buff.append("&lt;");
+			} else if (ch == '>') {
+				buff.append("&gt;");
+			} else {
+				buff.append(ch);
+			}
+		}
+		return buff.toString();
+	}
+
+	public static String noNull(String str){
+		if(str == null){
+			return "";
+		}
+		return str;
+	}
 }
