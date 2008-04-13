c2eef96749392e0257a2b8cd61a5d477e0d5f3cb
Rakesh Kalra <rkalra@users.sourceforge.net>
2008-04-13 00:19:24 +0000

    Fixed MetaDataWriter to handle null description in MBean metadata

--- management/src/main/java/org/jmanage/core/management/metadata/MetaDataWriter.java
+++ management/src/main/java/org/jmanage/core/management/metadata/MetaDataWriter.java
@@ -32,71 +32,70 @@ import org.jmanage.core.management.ObjectOperationInfo;
 import org.jmanage.core.management.ObjectParameterInfo;
 import org.jmanage.core.management.ServerConnection;
 import org.jmanage.core.management.ServerConnector;
+import org.jmanage.util.StringUtils;
 
 /**
  * Used to take dump of the MBean metadata from the server.
- *
+ * 
  * @author Rakesh Kalra
  */
 public class MetaDataWriter {
 
-    public static void main(String[] args) throws IOException{
-        
-        ApplicationConfig appConfig = 
-            ApplicationConfigFactory.create("1", "test", "local", 
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null);
-        ServerConnection connection = ServerConnector.getServerConnection(appConfig);
-        Set<ObjectName> objectNames = connection.queryNames(new ObjectName("*:*"));
-        //Set<ObjectName> sortedObjectNames = new TreeSet<ObjectName>();
-        //sortedObjectNames.addAll(objectNames);
-        Document document = getDocument(connection, objectNames);
-        /* write to the disc */
-        XMLOutputter writer = new XMLOutputter();
-        writer.output(document, new FileWriter("c:\\PlatformMBeans2.xml"));
-        connection.close();
-    }
+	public static void main(String[] args) throws IOException {
 
-    private static Document getDocument(ServerConnection connection, Set<ObjectName> objectNames){
-        Document doc = new Document();
-        Element rootElement = new Element("mbeans");
-        doc.setRootElement(rootElement);
-        /* mbeans */
-        for(ObjectName objName:objectNames){
-           ObjectInfo objInfo = connection.getObjectInfo(objName);
-           Element mbeanElement = new Element("mbean");
-           mbeanElement.setAttribute("name", objInfo.getObjectName().toString());
-           mbeanElement.setAttribute("description", objInfo.getDescription());
-           /* attributes */
-           for(ObjectAttributeInfo attrInfo:objInfo.getAttributes()){
-               Element attrElement = new Element("attribute");
-               attrElement.setAttribute("name", attrInfo.getName());
-               attrElement.setAttribute("description", attrInfo.getDescription());
-               mbeanElement.addContent(attrElement);
-           }
-           /* operations */
-           for(ObjectOperationInfo oprInfo:objInfo.getOperations()){
-               Element oprElement = new Element("operation");
-               oprElement.setAttribute("name", oprInfo.getName());
-               oprElement.setAttribute("description", oprInfo.getDescription());
-               /*parameters*/
-               for(ObjectParameterInfo paramInfo: oprInfo.getSignature()){
-                   Element paramElement = new Element("parameter");
-                   paramElement.setAttribute("name", paramInfo.getName());
-                   paramElement.setAttribute("type", paramInfo.getType());
-                   paramElement.setAttribute("description", paramInfo.getDescription());
-                   oprElement.addContent(paramElement);
-               }
-               mbeanElement.addContent(oprElement);
-           }
-           rootElement.addContent(mbeanElement);
-        }
-        return doc;
-    }
-    
+		ApplicationConfig appConfig = ApplicationConfigFactory.create("1", "test", "local",
+				null, null, null, null, null, null, null);
+		ServerConnection connection = ServerConnector.getServerConnection(appConfig);
+		Set<ObjectName> objectNames = connection.queryNames(new ObjectName("*:*"));
+		// Set<ObjectName> sortedObjectNames = new TreeSet<ObjectName>();
+		// sortedObjectNames.addAll(objectNames);
+		Document document = getDocument(connection, objectNames);
+		/* write to the disc */
+		XMLOutputter writer = new XMLOutputter();
+		writer.output(document, new FileWriter("c:\\PlatformMBeans2.xml"));
+		connection.close();
+	}
+
+	private static Document getDocument(ServerConnection connection,
+			Set<ObjectName> objectNames) {
+		Document doc = new Document();
+		Element rootElement = new Element("mbeans");
+		doc.setRootElement(rootElement);
+		/* mbeans */
+		for (ObjectName objName : objectNames) {
+			ObjectInfo objInfo = connection.getObjectInfo(objName);
+			Element mbeanElement = new Element("mbean");
+			mbeanElement.setAttribute("name", objInfo.getObjectName().toString());
+			mbeanElement.setAttribute("description", StringUtils.noNull(objInfo
+					.getDescription()));
+			/* attributes */
+			for (ObjectAttributeInfo attrInfo : objInfo.getAttributes()) {
+				Element attrElement = new Element("attribute");
+				attrElement.setAttribute("name", attrInfo.getName());
+				attrElement.setAttribute("description", StringUtils.noNull(attrInfo
+						.getDescription()));
+				mbeanElement.addContent(attrElement);
+			}
+			/* operations */
+			for (ObjectOperationInfo oprInfo : objInfo.getOperations()) {
+				Element oprElement = new Element("operation");
+				oprElement.setAttribute("name", oprInfo.getName());
+				oprElement.setAttribute("description", StringUtils.noNull(oprInfo
+						.getDescription()));
+				/* parameters */
+				for (ObjectParameterInfo paramInfo : oprInfo.getSignature()) {
+					Element paramElement = new Element("parameter");
+					paramElement.setAttribute("name", paramInfo.getName());
+					paramElement.setAttribute("type", paramInfo.getType());
+					paramElement.setAttribute("description", StringUtils.noNull(paramInfo
+							.getDescription()));
+					oprElement.addContent(paramElement);
+				}
+				mbeanElement.addContent(oprElement);
+			}
+			rootElement.addContent(mbeanElement);
+		}
+		return doc;
+	}
+
 }
